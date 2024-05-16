e9f8b6d10e136c3cc71fdbad96f69d1afca1e81f
hongjinkwon <hjk0472@gmail.com>
2024-05-16 20:15:46 +0900

    🐘Feat: 연동 애플리케이션 & postgresSQL

--- src/main/kotlin/com/teamsparta/todolist/domain/todo/dto/CreateTodoRequest.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/todo/dto/CreateTodoRequest.kt
@@ -1,7 +1,5 @@
 package com.teamsparta.todolist.domain.todo.dto
 
-import java.util.Date
-
 data class CreateTodoRequest(
     val title: String,
     val description: String?,
--- src/main/kotlin/com/teamsparta/todolist/domain/todo/dto/TodoResponse.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/todo/dto/TodoResponse.kt
@@ -1,8 +1,5 @@
 package com.teamsparta.todolist.domain.todo.dto
 
-import java.time.LocalDateTime
-import java.util.Date
-
 data class TodoResponse(
     val id: Long,
     val title: String,
